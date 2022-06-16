60d347663aa4bd7d9dceda82c0e8ff3423d0407e
Alexey Kostin <alexey-kostin@yandex.ru>
2022-06-16 12:42:55 +0300

    feat: сделать мобильную вёрстку

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+
+const options = {
+  controlColor: "#D1DE37",
+  addCircle: false,
+  addCircleBlur: false,
+
+};
+
+const viewers = document.querySelectorAll(".image-compare");
+
+viewers.forEach((element) => {
+  let view = new ImageCompare(element, options).mount();
+});
